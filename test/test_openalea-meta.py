c0b6a569bddef0997f449f90d79e1ee1c3eb6a2e
baugetfa <fabrice.bauget@cirad.fr>
2026-01-12 15:43:48 +0100

    [no ci] added openalea dependences

--- test/test_openalea-meta.py
+++ test/test_openalea-meta.py
@@ -1,3 +1,23 @@
 import pytest
 
-from openalea-meta import openalea-meta
+from openalea.plantgl.all import *
+from openalea.mtg.io import *
+from openalea.lpy import *
+from openalea.core import *
+from openalea.widgets import *
+from openalea.mtg import *
+from openalea.image import *
+from openalea.oalab import *
+from openalea.grapheditor import *
+from openalea.visualea import *
+from openalea.weberpenn import *
+from openalea.rsml import *
+from openalea.ratp import *
+from openalea.caribu import *
+from openalea.astk import *
+from openalea.adel import *
+from openalea.spice import *
+from openalea.hydroroot import *
+from openalea.phenomenal import *
+from openalea.hydroshoot import *
+from openalea.wheatfspm import *
